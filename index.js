f71ff2efcb6d9e8fdf3e0346e42ebb9e0051b0f6
D4ve-Code <78406260+D4ve-Code@users.noreply.github.com>
2023-01-18 14:23:55 +0100

    updateGoogleAuth

--- index.js
+++ index.js
@@ -145,19 +145,11 @@ app.get("/occupation/destroy", (req, res) => {
     const result = vilainsDatas.filter(data => data.occupation === "destroy")
     res.status(200).json(result);
 });
-// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
-app.listen(serverPort, (err) => {
-    if (err) {
-        console.log("ho crap!!");
-    } else {
-        console.log("http://localhost:4242")
-    }
-});
 
 //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX GOOGLE AUTH XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
 
 /**
  *  This function is used verify a google account
@@ -176,78 +168,90 @@ async function verifyGoogleToken(token) {
     return { error: "Invalid user detected. Please try again" };
   }
 }
-app.post("/signup", async (req, res) => {
-    try {
-      // console.log({ verified: verifyGoogleToken(req.body.credential) });
-      if (req.body.credential) {
-        const verificationResponse = await verifyGoogleToken(req.body.credential);
-  
-        if (verificationResponse.error) {
-          return res.status(400).json({
-            message: verificationResponse.error,
-          });
-        }
-  
-        const profile = verificationResponse?.payload;
-  
-        DB.push(profile);
-  
-        res.status(201).json({
-          message: "Signup was successful",
-          user: {
-            firstName: profile?.given_name,
-            lastName: profile?.family_name,
-            picture: profile?.picture,
-            email: profile?.email,
-            token: jwt.sign({ email: profile?.email }, "myScret", {
-              expiresIn: "1d",
-            }),
-          },
-        });
-      }
-    } catch (error) {
-      res.status(500).json({
-        message: "An error occurred. Registration failed.",
-      });
-    }
-  });
 
-  app.post("/login", async (req, res) => {
-    try {
-      if (req.body.credential) {
-        const verificationResponse = await verifyGoogleToken(req.body.credential);
-        if (verificationResponse.error) {
-          return res.status(400).json({
-            message: verificationResponse.error,
-          });
-        }
-  
-        const profile = verificationResponse?.payload;
-  
-        const existsInDB = DB.find((person) => person?.email === profile?.email);
-  
-        if (!existsInDB) {
-          return res.status(400).json({
-            message: "You are not registered. Please sign up",
-          });
-        }
-  
-        res.status(201).json({
-          message: "Login was successful",
-          user: {
-            firstName: profile?.given_name,
-            lastName: profile?.family_name,
-            picture: profile?.picture,
-            email: profile?.email,
-            token: jwt.sign({ email: profile?.email }, process.env.JWT_SECRET, {
-              expiresIn: "1d",
-            }),
-          },
+
+app.post("/signup", async (req, res) => {
+  try {
+    // console.log({ verified: verifyGoogleToken(req.body.credential) });
+    if (req.body.credential) {
+      const verificationResponse = await verifyGoogleToken(req.body.credential);
+      if (verificationResponse.error) {
+        return res.status(400).json({
+          message: verificationResponse.error,
         });
       }
-    } catch (error) {
-      res.status(500).json({
-        message: error?.message || error,
+
+      const profile = verificationResponse?.payload;
+
+      DB.push(profile);
+
+      res.status(201).json({
+        message: "Signup was successful",
+        user: {
+          firstName: profile?.given_name,
+          lastName: profile?.family_name,
+          picture: profile?.picture,
+          email: profile?.email,
+          token: jwt.sign({ email: profile?.email }, "myScret", {
+            expiresIn: "1d",
+          }),
+        },
       });
     }
-  });
+  } catch (error) {
+    res.status(500).json({
+      message: "An error occured. Registration failed.",
+    });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    if (req.body.credential) {
+      const verificationResponse = await verifyGoogleToken(req.body.credential);
+      if (verificationResponse.error) {
+        return res.status(400).json({
+          message: verificationResponse.error,
+        });
+      }
+
+      const profile = verificationResponse?.payload;
+
+      const existsInDB = DB.find((person) => person?.email === profile?.email);
+
+      if (!existsInDB) {
+        return res.status(400).json({
+          message: "You are not registered. Please sign up",
+        });
+      }
+
+      res.status(201).json({
+        message: "Login was successful",
+        user: {
+          firstName: profile?.given_name,
+          lastName: profile?.family_name,
+          picture: profile?.picture,
+          email: profile?.email,
+          token: jwt.sign({ email: profile?.email }, process.env.JWT_SECRET, {
+            expiresIn: "1d",
+          }),
+        },
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: error?.message || error,
+    });
+  }
+});
+
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+app.listen(serverPort, (err) => {
+    if (err) {
+        console.log("ho crap!!");
+    } else {
+        console.log("http://localhost:4242")
+    }
+});
